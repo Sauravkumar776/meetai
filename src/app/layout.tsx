3a61059589d3061328bdbeb0f740026477270bcf
saurav <saurav@tangentmotorsport.com>
2025-09-24 16:18:58 +0530

    feat: trpc setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { TRPCReactProvider } from "@/trpc/client";
 import "./globals.css";
 
 const inter = Inter({
@@ -23,12 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.variable} ${interMono.variable} antialiased`}
-      >
-        {children}
+    <TRPCReactProvider>
+      <html lang="en">
+        <body
+          className={`${inter.variable} ${interMono.variable} antialiased`}
+        >
+          {children}
       </body>
     </html>
+    </TRPCReactProvider>
   );
 }
--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,41 +1,16 @@
 "use client"
 
-import { useState } from "react"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { authClient } from "@/lib/auth-client"
-import { useRouter } from "next/navigation"
+import { useTRPC } from "@/trpc/client"
+import { useQuery } from "@tanstack/react-query";
 
 export const HomeView = () => {
 
-  const { data: session } = authClient.useSession()
-  const router = useRouter();
-
-  if (!session) {
-    return (
-      <div>
-        Loading...
-      </div>
-    )
-  }
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.hello.queryOptions({ text: "from tRPC" }));
 
   return (
-      <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-slate-100 to-slate-200 px-4">
-        <div className="w-full max-w-md bg-white shadow-xl rounded-2xl p-8 text-center space-y-4">
-          <h2 className="text-3xl font-bold">Welcome, {session.user?.name || session.user?.email}!</h2>
-          <p className="text-gray-600">You are logged in.</p>
-          <Button variant="destructive" onClick={() => authClient.signOut({
-            fetchOptions: {
-                onSuccess: () => {
-                    router.push("/sign-in")
-                }
-            }
-          })}
-          >
-            Sign Out
-          </Button>
-        </div>
-      </div>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-slate-100 to-slate-200 px-4">
+      {data?.greeting}
+    </div>
   )
 }
